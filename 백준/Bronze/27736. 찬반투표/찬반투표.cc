05bc7949d4a3a946ea331653c549f2df43b20677
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2024-06-10 22:53:09 +0900

    [Bronze III] Title: 찬반투표, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27736. 찬반투표/찬반투표.cc
+++ 백준/Bronze/27736. 찬반투표/찬반투표.cc
@@ -11,8 +11,19 @@ int main() {
         else if (p == -1) x[2]++;
         else x[1]++;
     }
-    if (x[1] > n / 2) cout << "INVALID";
-    else if (x[2] > x[0] || x[2] == x[0]) cout << "REJECTED";
+    if (n % 2 == 0) {
+        if (x[1] >= n / 2) {
+            cout << "INVALID";
+            return 0;
+        }
+    }
+    else {
+        if (x[1] >= n / 2 + 1) {
+            cout << "INVALID";
+            return 0;
+        }
+    }
+    if (x[2] > x[0] || x[2] == x[0]) cout << "REJECTED";
     else cout << "APPROVED";
 
     return 0;
