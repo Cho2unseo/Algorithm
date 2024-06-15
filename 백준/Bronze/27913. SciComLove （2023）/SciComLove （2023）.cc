33ec977f2eb431cee2cfcd7dc7661f6e9aa8207a
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2024-06-15 13:51:57 +0900

    [Bronze II] Title: SciComLove (2023), Time: 44 ms, Memory: 2624 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27913. SciComLove （2023）/SciComLove （2023）.cc
+++ 백준/Bronze/27913. SciComLove （2023）/SciComLove （2023）.cc
@@ -4,19 +4,27 @@
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     string s = "SciComLove", news = "";
-    int n, q;
+    int n, q, cnt = 0;
     cin >> n >> q;
     for (int i = 0; i < n / 10; i++) news += s;
     news += s.substr(0, n % 10);
+    for (char c: news) {
+        if (isupper(c)) cnt++;
+    }
     for (int i = 1; i <= q; i++) {
-        int cnt = 0;
         int x;
         cin >> x;
-        if (news[x - 1] >= 'a' && news[x - 1] <= 'z') news[x - 1] = news[x - 1] - ('a' - 'A');
-        else if (news[x - 1] >= 'A' && news[x - 1] <= 'Z') news[x - 1] = news[x - 1] + ('a' - 'A');
-        for (int j = 0; j < news.length(); j++) {
-            if (news[j] >= 'A' && news[j] <= 'Z') cnt++;
+        if (islower(news[x - 1])) {
+            news[x - 1] = toupper(news[x - 1]);
+            cnt++;
+        }
+        else if (isupper(news[x - 1])) {
+            news[x - 1] = tolower(news[x - 1]);
+            cnt--;
         }
         cout << cnt << "\n";
     }
