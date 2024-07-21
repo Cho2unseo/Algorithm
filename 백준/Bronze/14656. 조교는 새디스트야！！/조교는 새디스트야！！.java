e664d01ed256f6d38a8062323a84230e50594b53
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2024-07-21 13:27:31 +0900

    [Bronze III] Title: 조교는 새디스트야!!, Time: 192 ms, Memory: 16908 KB -BaekjoonHub

---
+++ 백준/Bronze/14656. 조교는 새디스트야！！/조교는 새디스트야！！.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] x = new int[n];
+        int cnt = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            x[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0; i < n; i++) {
+            if (x[i] != (i + 1)) cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
