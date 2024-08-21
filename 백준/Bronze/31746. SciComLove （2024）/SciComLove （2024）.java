fdf20a1294df31c769d52a943c6445b3ba14eb57
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2024-08-21 22:42:59 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 168 ms, Memory: 17700 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        if (n % 2 == 0) System.out.println("SciComLove");
+        else System.out.println("evoLmoCicS");
+    }
+}
