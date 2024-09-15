8740e6e231594fd6ff9173439c02fd37da5540d0
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2024-09-15 22:00:10 +0900

    [Bronze III] Title: 괴짜 교수, Time: 152 ms, Memory: 15068 KB -BaekjoonHub

---
+++ 백준/Bronze/11109. 괴짜 교수/괴짜 교수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int d = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+            if (d + n * p == s * n) System.out.println("does not matter");
+            else if (d + n * p < s * n) System.out.println("parallelize");
+            else System.out.println("do not parallelize");
+        }
+    }
+}
