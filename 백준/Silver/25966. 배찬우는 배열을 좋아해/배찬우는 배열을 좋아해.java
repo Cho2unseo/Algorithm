e676dcd8fd1190d0256456ccee5be3003e28804e
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2024-09-17 22:25:52 +0900

    [Silver V] Title: 배찬우는 배열을 좋아해, Time: 1952 ms, Memory: 371384 KB -BaekjoonHub

---
+++ 백준/Silver/25966. 배찬우는 배열을 좋아해/배찬우는 배열을 좋아해.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int f = Integer.parseInt(st.nextToken());
+            int ii = Integer.parseInt(st.nextToken());
+            int jj = Integer.parseInt(st.nextToken());
+            if (f == 0) {
+                int kk = Integer.parseInt(st.nextToken());
+                arr[ii][jj] = kk;
+            }
+            else if (f == 1) {
+                int[] tmp = arr[ii];
+                arr[ii] = arr[jj];
+                arr[jj] = tmp;
+            }
+        }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                bw.write(arr[i][j] + " ");
+            }
+            bw.newLine();
+        }
+        bw.flush();
+    }
+}
