4ae0f5fffd025fea6a67d5b32a871f77847798b0
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2024-09-20 19:01:13 +0900

    [Bronze III] Title: 수박 게임, Time: 168 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/31868. 수박 게임/수박 게임.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        for (int i = 0; i < n - 1; i++) {
+            k = k / 2;
+        }
+        System.out.println(k);
+    }
+}
