f15ba4437bc49de8f24f800fb5119073858f1576
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2024-10-19 21:00:07 +0900

    [Bronze IV] Title: 학식 사주기, Time: 168 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/31821. 학식 사주기/학식 사주기.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int m = sc.nextInt();
+        int sum = 0;
+        for (int i = 0; i < m; i++) {
+            int tmp = sc.nextInt();
+            sum += arr[tmp - 1];
+        }
+        System.out.println(sum);
+    }
+}
