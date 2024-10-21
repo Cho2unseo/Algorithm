a2341bd307f6c3c2ecc0b8e0c15f84ede4f2c112
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2024-10-21 20:01:15 +0900

    [Silver I] Title: 경로 찾기, Time: 292 ms, Memory: 18136 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] g = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                g[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (g[i][k] == 1 && g[k][j] == 1) g[i][j] = 1;
+                }
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) System.out.print(g[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
