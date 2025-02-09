7f8662c61247d48a627a1c884012ab5923d5478a
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-02-09 19:57:35 +0900

    [Bronze V] Title: Pyramids, Time: 100 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Bronze/5341. Pyramids/Pyramids.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) break;
+            int cnt = (n + 1) * n / 2;
+            System.out.println(cnt);
+        }
+    }
+}
