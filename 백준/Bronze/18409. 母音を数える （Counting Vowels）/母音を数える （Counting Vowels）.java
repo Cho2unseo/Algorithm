c03c2469c657966b0a336a9ebebd1490c7059338
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-03-14 22:42:52 +0900

    [Bronze V] Title: 母音を数える (Counting Vowels), Time: 168 ms, Memory: 17500 KB -BaekjoonHub

---
+++ 백준/Bronze/18409. 母音を数える （Counting Vowels）/母音を数える （Counting Vowels）.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String s = sc.nextLine();
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
