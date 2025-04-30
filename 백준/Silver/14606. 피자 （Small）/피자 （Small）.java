84eeff02456b134a393792790ff0f40e17536bda
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-04-30 17:27:33 +0900

    [Silver V] Title: 피자 (Small), Time: 172 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Silver/14606. 피자 （Small）/피자 （Small）.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n + 1];
+        arr[1] = 0;
+        if (n == 1) {
+            System.out.println(0);
+            return;
+        }
+        for (int i = 2; i <= n; i++) arr[i] = arr[i - 1] + (i - 1);
+        System.out.println(arr[n]);
+    }
+}
