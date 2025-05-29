52bfb3e19041d45bb74935ede1af3119dfa5bc50
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-05-29 23:28:33 +0900

    [level 2] Title: 괄호 회전하기, Time: 52.10 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -5,27 +5,28 @@ class Solution {
     static boolean check(String s) {
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '{' || s.charAt(i) == '[' || s.charAt(i) == '(') {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
                 stack.push(s.charAt(i));
             }
             else {
                 if (stack.isEmpty()) return false;
                 char top = stack.pop();
-                if (s.charAt(i) == '}') {
+                if (s.charAt(i) == ')') {
+                    if (top != '(') return false;
+                }
+                else if (s.charAt(i) == '}') {
                     if (top != '{') return false;
                 }
                 else if (s.charAt(i) == ']') {
                     if (top != '[') return false;
                 }
-                else if (s.charAt(i) == ')') {
-                    if (top != '(') return false;
-                }
+            
             }
         }
         if (!stack.isEmpty()) return false;
         return true;
     }
-    
+        
     public int solution(String s) {
         int answer = 0;
         String newS = s + s;
