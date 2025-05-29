7cbf9f2335390b74a77eb269def1c9d41bb18fcc
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-05-29 17:23:54 +0900

    [level 3] Title: 단어 변환, Time: 0.67 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
 
 ### 성능 요약
 
-메모리: 74.5 MB, 시간: 0.91 ms
+메모리: 73.6 MB, 시간: 0.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 23일 11:13:05
+2025년 05월 29일 17:23:52
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,6 +1,6 @@
 import java.util.*;
+
 class Solution {
-    
     class Word {
         String word;
         int count;
@@ -10,33 +10,28 @@ class Solution {
         }
     }
     
-    int getDiffCount(String word1, String word2) {
+    static int diff(String s1, String s2) {
         int count = 0;
-        for (int i = 0; i < word1.length(); i++)
-            if (word1.charAt(i) != word2.charAt(i)) count++;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) count++;
+        }
         return count;
     }
     
     public int solution(String begin, String target, String[] words) {
-        // BFS로 최단 거리 구하기
-        // 시작점 예약 (문자열 begin)
-        Set<String> visited = new HashSet<>();
+        int answer = 0;
+        Set<String> isVisited = new HashSet<>();
         Queue<Word> queue = new ArrayDeque<>();
+        isVisited.add(begin);
         queue.offer(new Word(begin, 0));
-        visited.add(begin);
-        // boolean[] visited = new boolean[words.length];
-        // begin으로 가는 경우는 없으니 visited 처리 해줊 필요 X
         while (!queue.isEmpty()) {
-            // 현재 노드 방문
             Word cur = queue.poll();
-            // if 현재 노드 == target: return count;
-            if (cur.word.equals(target)) return cur.count;
-            // 다음 노드 방문
-            for (String next: words) {
-                if (getDiffCount(cur.word, next) == 1) {
-                    if (!visited.contains(next)) {
-                        queue.offer(new Word(next, cur.count + 1));
-                        visited.add(next);
+            if (target.equals(cur.word)) return cur.count;
+            for (String s: words) {
+                if (diff(s, cur.word) == 1) {
+                    if (!isVisited.contains(s)) {
+                        queue.offer(new Word(s, cur.count + 1));
+                        isVisited.add(s);
                     }
                 }
             }
