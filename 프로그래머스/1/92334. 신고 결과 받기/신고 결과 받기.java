43865ed893a702b6562021de89c6ef06730603aa
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-05-30 09:46:52 +0900

    [level 1] Title: 신고 결과 받기, Time: 243.61 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,26 +2,24 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
         Set<String> set = new HashSet<>(Arrays.asList(report));
-        Map<String, List<String>> reportMap = new HashMap<>();
+        Map<String, List<String>> iReportWho = new HashMap<>();
         Map<String, Integer> reportCount = new HashMap<>();
         for (String s: set) {
             String[] str = s.split(" ");
             String from = str[0];
             String to = str[1];
             reportCount.put(to, reportCount.getOrDefault(to, 0) + 1);
-            if (!reportMap.containsKey(from)) {
-                reportMap.put(from, reportMap.getOrDefault(from, new ArrayList<>()));
+            if (!iReportWho.containsKey(from)) {
+                iReportWho.put(from, iReportWho.getOrDefault(from, new ArrayList<>()));
             }
-            reportMap.get(from).add(to);   
+            iReportWho.get(from).add(to);
         }
-        int[] answer = new int[id_list.length];
         for (int i = 0; i < id_list.length; i++) {
             String name = id_list[i];
-            if (reportMap.containsKey(name)) {
-                                    // System.out.println("신고자" + name);
-
-                for (String user: reportMap.get(name)) {
+            if (iReportWho.containsKey(name)) {
+                for (String user: iReportWho.get(name)) {
                     // System.out.println(user);
                     if (reportCount.get(user) >= k) answer[i]++;
                 }
