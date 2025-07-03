dc6ec12a2a2e9bf5001f20e4fe4673e4fe1182ff
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-07-03 16:31:30 +0900

    [level 2] Title: 소수 찾기, Time: 22.88 ms, Memory: 98 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    
+    static Set<Integer> set;
+    
+    boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+    
+    void backtrack(String numbers, boolean[] isVisited, String cur) {
+        if (cur.length() != 0) {
+            int num = Integer.parseInt(cur);
+            if (isPrime(num)) set.add(num);
+        }
+        for (int i = 0; i < numbers.length(); i++) {
+            if (!isVisited[i]) {
+                isVisited[i] = true;
+                backtrack(numbers, isVisited, cur + numbers.charAt(i));
+                isVisited[i] = false;
+            }
+        }
+    }
+    
+    public int solution(String numbers) {
+        set = new HashSet<>();
+        boolean[] isVisited = new boolean[numbers.length()];
+        backtrack(numbers, isVisited, "");
+        return set.size();
+    }
+}
