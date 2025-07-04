2f95a0bee5443e95e2a14b611d48989db472772e
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-07-04 13:29:19 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.57 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -7,20 +7,19 @@ class Solution {
             if (a[1] != b[1]) return a[1] - b[1];
             return a[0] - b[0];
         });
-        int time = 0; // 현재 시간
-        int idx = 0; // jobs 배열에서 현재 확인할 작업
-        int total = 0; // 총 반환 시간
-        int count = 0; // 완료된 작업 수
-        
+        int count = 0;
+        int idx = 0;
+        int time = 0;
+        int total = 0;
         while (count < jobs.length) {
-            while (idx < jobs.length && jobs[idx][0] <= time) {
+            while (idx < jobs.length && time >= jobs[idx][0]) {
                 pq.add(jobs[idx]);
                 idx++;
             }
             if (!pq.isEmpty()) {
-                int[] job = pq.poll(); // 우선 순위 가장 높은 작업 꺼냄
-                time += job[1]; // 작업 수행 후 시간 증가
-                total += time - job[0]; // 반환 시간 = 종료 - 요청
+                int[] job = pq.remove();
+                time += job[1];
+                total += time - job[0];
                 count++;
             } else {
                 time = jobs[idx][0];
