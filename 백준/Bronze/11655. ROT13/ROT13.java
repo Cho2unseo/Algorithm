b78c885d2f33f184fb2310a5132077ef9feda688
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-08-09 23:42:05 +0900

    [Bronze I] Title: ROT13, Time: 108 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        char[] arr = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        char[] arr2 = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') sb.append(arr[(s.charAt(i) - 'A' + 13) % 26]);
+            else if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') sb.append(arr2[(s.charAt(i) - 'a' + 13) % 26]);
+            else sb.append(s.charAt(i));
+        }
+        System.out.println(sb);
+    }
+}
