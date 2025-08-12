621486be8638262d16e842e2353aeb22bb3c597a
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-08-12 22:55:52 +0900

    [Silver III] Title: 수리공 항승, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+        int cnt = 0;
+        double tmp = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > tmp) {
+                cnt++;
+                tmp = arr[i] - 0.5 + l;
+            }
+        }
+        System.out.println(cnt);
+
+    }
+}
