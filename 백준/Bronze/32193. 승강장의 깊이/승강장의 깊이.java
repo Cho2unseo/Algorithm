2824df252f074638ca9326edfebda434c03b40bb
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-08-27 20:05:54 +0900

    [Bronze III] Title: 승강장의 깊이, Time: 1560 ms, Memory: 80508 KB -BaekjoonHub

---
+++ 백준/Bronze/32193. 승강장의 깊이/승강장의 깊이.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            a += Integer.parseInt(st.nextToken());
+            b += Integer.parseInt(st.nextToken());
+            System.out.println(a - b);
+        }
+    }
+}
