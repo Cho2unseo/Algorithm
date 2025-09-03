27f207806aa4b9684d434ecf9ff49d52d71ffe55
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-09-03 23:30:13 +0900

    [Silver V] Title: Costume Party, Time: 1844 ms, Memory: 18608 KB -BaekjoonHub

---
+++ 백준/Silver/6159. Costume Party/Costume Party.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                if (arr[i] + arr[j] <= s) cnt++;
+            }
+        }
+        System.out.println(cnt / 2);
+    }
+}
