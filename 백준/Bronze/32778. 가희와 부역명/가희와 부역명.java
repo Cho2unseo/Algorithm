61a7b3658ef5721b443643b1102a682d04198610
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-09-12 22:28:17 +0900

    [Bronze III] Title: 가희와 부역명, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/32778. 가희와 부역명/가희와 부역명.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        if (s.contains("(")) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '(') {
+                    System.out.println();
+                    continue;
+                }
+                else if (s.charAt(i) == ')') break;
+                System.out.print(s.charAt(i));
+            }
+        }
+        else System.out.println(s + "\n-");
+    }
+}
