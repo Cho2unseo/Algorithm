ecb9cc046c38d2433885408c3380f26499dd580b
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-10-01 20:50:37 +0900

    [Bronze II] Title: 비교 연산자, Time: 520 ms, Memory: 24352 KB -BaekjoonHub

---
+++ 백준/Bronze/5656. 비교 연산자/비교 연산자.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int idx = 1;
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            String s = st.nextToken();
+            int b = Integer.parseInt(st.nextToken());
+            if (s.equals("E")) break;
+            System.out.print("Case " + idx++ + ": ");
+            if (s.equals(">")) System.out.println(a > b);
+            else if (s.equals(">=")) System.out.println(a >= b);
+            else if (s.equals("<")) System.out.println(a < b);
+            else if (s.equals("<=")) System.out.println(a <= b);
+            else if (s.equals("==")) System.out.println(a == b);
+            else System.out.println(a != b);
+        }
+    }
+}
