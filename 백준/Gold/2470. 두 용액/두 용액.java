5c99de5e60ee694a7974412d8d5f43e8b5cb3538
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-10-13 20:04:18 +0900

    [Gold V] Title: 두 용액, Time: 332 ms, Memory: 28888 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+        int left = 0;
+        int right = n - 1;
+        int min = Integer.MAX_VALUE;
+        int a = 0;
+        int b = 0;
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (Math.abs(sum) < min) {
+                min = Math.min(Math.abs(min), Math.abs(sum));
+                a = arr[left];
+                b = arr[right];
+            }
+            if (sum < 0) left++;
+            else right--;
+        }
+        System.out.println(a + " " + b);
+    }
+}
