9b52729afebffd02bcc07a03af1a5b9ed2e54ac4
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-12-15 23:52:51 +0900

    [Bronze V] Title: Счастье Мистера Бина, Time: 116 ms, Memory: 14480 KB -BaekjoonHub

---
+++ 백준/Bronze/29163. Счастье Мистера Бина/Счастье Мистера Бина.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            if (a % 2 == 0) cnt++;
+        }
+        System.out.println(cnt > n - cnt ? "Happy" : "Sad");
+    }
+}
