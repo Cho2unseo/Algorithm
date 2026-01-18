d5490cf1fde87ffa11fc64150d48870237866f34
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2026-01-18 23:49:33 +0900

    [Silver V] Title: 폴리오미노, Time: 164 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        s = s.replace("XXXX", "AAAA");
+        s = s.replace("XX", "BB");
+        if (!s.contains("X")) System.out.println(s);
+        else System.out.println(-1);
+    }
+}
