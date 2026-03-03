84a3a950d4e84d846118bd6a1d9bc7427be548de
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2026-03-03 23:41:52 +0900

    [Silver I] Title: IOIOI, Time: 248 ms, Memory: 20156 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        int res = 0;
+        int cnt = 0;
+        for (int i = 1; i < m - 1; i++) {
+            if (s.charAt(i - 1) == 'I' && s.charAt(i) == 'O' && s.charAt(i + 1) == 'I') {
+                cnt++;
+                if (cnt == n) {
+                    res++;
+                    cnt--;
+                }
+                i++;
+            }
+            else cnt = 0;
+        }
+        System.out.println(res);
+    }
+}
