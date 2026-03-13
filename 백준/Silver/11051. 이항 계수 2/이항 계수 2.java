edd4570da06cbdb6cff76434bcf4cea88db8937a
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2026-03-13 23:43:13 +0900

    [Silver II] Title: 이항 계수 2, Time: 208 ms, Memory: 25560 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        long[][] arr = new long[n + 1][n + 1];
+        for (int i = 0; i <= n; i++) {
+            arr[i][0] = 1;
+            arr[i][1] = i;
+            arr[i][i] = 1;
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) arr[i][j] = (arr[i - 1][j] + arr[i - 1][j - 1]) % 10007;
+        }
+        System.out.println(arr[n][k] % 10007);
+    }
+}
