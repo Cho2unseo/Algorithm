79bbcf081a7419e30c4ce58eb597848a434c2079
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2026-03-21 23:33:20 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 344 ms, Memory: 21744 KB -BaekjoonHub

---
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            if (i == n) {
+                for (int j = 1; j <= 2 * n - 1; j++) System.out.print("*");
+                break;
+            }
+            else {
+                for (int j = 1; j <= n - i; j++) System.out.print(" ");
+                for (int j = 1; j <= 2 * i - 1; j++) {
+                    if (j == 1 || j == 2 * i - 1) System.out.print("*");
+                    else System.out.print(" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
