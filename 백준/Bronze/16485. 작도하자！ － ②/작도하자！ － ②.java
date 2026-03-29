bc5190d05f2b3978e501856033ec4394ce01630a
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2026-03-29 23:56:37 +0900

    [Bronze II] Title: 작도하자! - ②, Time: 172 ms, Memory: 17740 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Bronze/16485. 작도하자！ － ②/작도하자！ － ②.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+        int b = sc.nextInt();
+        double res = (double) c / b;
+        System.out.println(res);
+    }
+}
